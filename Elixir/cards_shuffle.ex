560b80996326f966e8ef12397995c51344c1669c
Liz Torres <40001343+elitorresr@users.noreply.github.com>
2018-10-26 12:09:50 -0500

    Upload Elixir shuffle
    Last try? ¯\_(ツ)_/¯

---
+++ Elixir/cards_shuffle.ex
@@ -0,0 +1,33 @@
+defmodule Cards do
+  def create_deck do
+    values = ["ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "jack", "queen", "king"]
+    suits = ["spades", "clubs", "hearts", "diamonds"]
+     for suit <- suits, value <- values do
+      "#{value} #{suit}"
+    end
+  end
+   def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+   def contains?(deck, card)  do
+    Enum.member?(deck, card)
+  end
+   def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+   def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+     File.write(filename, binary)
+  end
+   def load(filename) do
+    case File.read(filename) do
+       {:ok, binary} -> :erlang.binary_to_term binary
+       {:error, _reason} -> "That file does not exist"
+    end
+  end
+   def create_hand(hand_size) do
+    Cards.create_deck
+      |> Cards.shuffle
+        |> Cards.deal(hand_size)
+   end
+end
